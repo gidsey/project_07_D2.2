ac1be87fecff6fe807a0de9c514e8919f6db4aa4
chrisguy <chris@chrisguy.co>
2019-09-24 19:09:18 +0100

    Password Strength Meter added (needs configuring)

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,57 +1,69 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-  $('textarea').autogrow({onInitialize: true});
-
-
-  //Cloner for infinite input lists
-  $(".circle--clone--list").on("click", ".circle--clone--add", function(){
-    var parent = $(this).parent("li");
-    var copy = parent.clone();
-    parent.after(copy);
-    copy.find("input, textarea, select").val("");
-    copy.find("*:first-child").focus();
-  });
-
-  $(".circle--clone--list").on("click", "li:not(:only-child) .circle--clone--remove", function(){
-    var parent = $(this).parent("li");
-    parent.remove();
-  });
-
-  // Adds class to selected item
-  $(".circle--pill--list a").click(function() {
-    $(".circle--pill--list a").removeClass("selected");
-    $(this).addClass("selected");
-  });
-
-  // Adds class to parent div of select menu
-  $(".circle--select select").focus(function(){
-   $(this).parent().addClass("focus");
-   }).blur(function(){
-     $(this).parent().removeClass("focus");
-   });
-
-  // Clickable table row
-  $(".clickable-row").click(function() {
-      var link = $(this).data("href");
-      var target = $(this).data("target");
-
-      if ($(this).attr("data-target")) {
-        window.open(link, target);
-      }
-      else {
-        window.open(link, "_self");
-      }
-  });
-
-  // Custom File Inputs
-  var input = $(".circle--input--file");
-  var text = input.data("text");
-  var state = input.data("state");
-  input.wrap(function() {
-    return "<a class='button " + state + "'>" + text + "</div>";
-  });
+    $('textarea').autogrow({onInitialize: true});
 
 
+    //Cloner for infinite input lists
+    $(".circle--clone--list").on("click", ".circle--clone--add", function () {
+        var parent = $(this).parent("li");
+        var copy = parent.clone();
+        parent.after(copy);
+        copy.find("input, textarea, select").val("");
+        copy.find("*:first-child").focus();
+    });
 
+    $(".circle--clone--list").on("click", "li:not(:only-child) .circle--clone--remove", function () {
+        var parent = $(this).parent("li");
+        parent.remove();
+    });
+
+    // Adds class to selected item
+    $(".circle--pill--list a").click(function () {
+        $(".circle--pill--list a").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    // Adds class to parent div of select menu
+    $(".circle--select select").focus(function () {
+        $(this).parent().addClass("focus");
+    }).blur(function () {
+        $(this).parent().removeClass("focus");
+    });
+
+    // Clickable table row
+    $(".clickable-row").click(function () {
+        var link = $(this).data("href");
+        var target = $(this).data("target");
+
+        if ($(this).attr("data-target")) {
+            window.open(link, target);
+        } else {
+            window.open(link, "_self");
+        }
+    });
+
+    // Custom File Inputs
+    var input = $(".circle--input--file");
+    var text = input.data("text");
+    var state = input.data("state");
+    input.wrap(function () {
+        return "<a class='button " + state + "'>" + text + "</div>";
+    });
+
+    // Password Strength Meter
+    $('#id_new_password').password({
+        minimumLength: 14, // minimum password length (below this threshold, the score is 0)
+        showPercent: false
+    });
+
+    $('#id_new_password').on('password.score', (e, score) => {
+        console.log('Called every time a new score is calculated (this means on every keyup)')
+        console.log('Current score is %d', score)
+    })
+
+    $('#id_new_password').on('password.text', (e, text, score) => {
+        console.log('Called every time the text is changed (less updated than password.score)')
+        console.log('Current message is %s with a score of %d', text, score)
+    })
 
 });
