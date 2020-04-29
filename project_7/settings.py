52a4878e3455dfa2fa2a3e6fe1506996aba4d7e2
gidsey <chris@chrisguy.co>
2020-04-29 14:33:22 +0100

    add media to STATICFILES_DIRS

--- project_7/settings.py
+++ project_7/settings.py
@@ -150,6 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 MEDIA_URL = '/media/'
