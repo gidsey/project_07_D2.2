04dd405d700ce6271e793824f3fa1e099fb9460b
gidsey <chris@chrisguy.co>
2020-04-29 14:10:13 +0100

    add postres db settings

--- project_7/deploy_settings/__init__.py
+++ project_7/deploy_settings/__init__.py
@@ -1,4 +1,6 @@
+import dj_database_url
 from project_7.settings import *
+
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -9,3 +11,6 @@ ALLOWED_HOSTS = [
 ]
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
