0361a640df384e84b686f22b3ad4949a05d18ad1
gidsey <chris@chrisguy.co>
2020-04-29 14:54:52 +0100

    add MEDIAFILES_DIRS

--- project_7/settings.py
+++ project_7/settings.py
@@ -150,11 +150,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
-    os.path.join(BASE_DIR, 'media'),
 ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media'),
+]
+
 
 # Sumernote WYSIWYG Editor
 # https://summernote.org/
